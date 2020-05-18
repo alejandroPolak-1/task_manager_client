5a903211e4395a740744fd239ea3f156c789b1be
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-18 15:37:49 -0300

    (Create Component and Routing) import (Router, Switch, Route) [react-router-dom] [ADD comp in Auth] Login,NewAccount, Projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NewAccount from './components/auth/Login.auth'
+import Login from './components/auth/NewAccount.auth'
+import Projects from './components/projects/Projects'
+
 
 function App() {
   return (
-    <h1>Task Manager</h1>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/new-account" component={NewAccount} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
