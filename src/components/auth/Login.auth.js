f35b6393ca487bf15912adf8f49e2361cd0f9694
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-21 09:43:05 -0300

    [Login state] [import Link from react-router-dom Link for get new account

--- src/components/auth/Login.auth.js
+++ src/components/auth/Login.auth.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  const handleOnChange = () => {}
+  //State to Log in
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  //Extract from user
+  const { email, password } = user
+
+  const handleOnChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    })
+    console.log(user)
+  }
+
+  //When the user wants to login
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    //validate(that there are no empty fields)
+
+
+    //pass to action
+  }
 
   return (
     <div className="form-usuario">
       <div className="contenedor-form sombra-dark">
         <h1>Log in</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="campo-form">
             <label htmlFor="email">Email</label>
             <input
@@ -16,6 +42,7 @@ const Login = () => {
               id="email"
               name="email"
               placeholder="Your e-mail"
+              value={email}
               onChange={handleOnChange}
             />
           </div>
@@ -27,6 +54,7 @@ const Login = () => {
               id="password"
               name="password"
               placeholder="Your password"
+              value={password}
               onChange={handleOnChange}
             />
           </div>
@@ -39,6 +67,10 @@ const Login = () => {
             />
           </div>
         </form>
+
+        <Link to={'/new-account'} className="enlace-cuenta">
+            Get new account
+        </Link>
       </div>
     </div>
   )
