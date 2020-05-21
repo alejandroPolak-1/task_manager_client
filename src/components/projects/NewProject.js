a55c263aa2ba8103dda6cb5263f32be9ba4b6f1f
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-21 18:54:20 -0300

    [State and function to new project]

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,18 +1,50 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const NewProject = () => {
+  //State for Project
+  const [project, setProject] = useState({
+    name: '',
+  })
+
+  //Extract name project
+  const { name } = project
+
+  //Save project, read the content of the project
+  const handleOnChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  //when the user submit from input
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    //Validate the project
+
+    //Add to state
+
+    //restart the form
+  }
+
   return (
     <Fragment>
-      <button type="button" className="btn btn-block btn-primario">
+      <button 
+        type="button" 
+         className="btn btn-block btn-primario"
+         >
         New Project
       </button>
 
-      <form className="formulario-nuevo-proyecto">
+      <form className="formulario-nuevo-proyecto" onSubmit={handleOnSubmit}>
         <input
           type="text"
           className="input-text"
           placeholder="Project Name"
           name="name"
+          value={name}
+          onChange={handleOnChange}
         />
 
         <input
