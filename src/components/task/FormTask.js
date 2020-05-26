79b53d17d4762a2a73c66804bb720c9720491b2d
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-26 10:27:34 -0300

    [Create Task for Project] (INsert tasks into the actual project) 1 (FormTask) -import useState, an add new State [task,setTask] - add onSubmit to form - add handleChange -> to read form values (target name & value) -import taskContext, for use addTasks() -add new Task-> addTasks(), to name same.    *task.projectId = actualProject.id (for identify the project)    *task.state= false (for init in false)    *pass new task-> "task"

--- src/components/task/FormTask.js
+++ src/components/task/FormTask.js
@@ -1,27 +1,65 @@
-import React, {useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import projectContext from '../../context/projects/projectContext'
+import taskContext from '../../context/tasks/taskContext'
 
 const FormTask = () => {
+  //Get if a project is active
+  const projectsContext = useContext(projectContext)
+  const { project } = projectsContext
 
-   //Get if a project is active
-   const projectsContext = useContext(projectContext)
-   const {project} = projectsContext
+   //Get function contex task
+   const tasksContext = useContext(taskContext)
+   const { addTask } = tasksContext
 
-   //If there is no select project, don't show FormTask
-  if(!project) return null
+  //State to the form
+  const [task, setTask] = useState({
+    name: '',
+  })
 
-   //to extract the current project
-   const [actualProject] = project
+  //Extract the name to task _> for value to input
+  const { name } = task
+
+  //If there is no select project, don't show FormTask
+  if (!project) return null
+
+  //read form values
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  //to extract the current project
+  const [actualProject] = project
+
+  //
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    //validate
+
+    //pass to validation
+
+    //add new Task to state of task
+    task.projectId = actualProject.id
+    task.state= false
+    addTask(task)
+
+    // reset form
+  }
 
   return (
     <div className="formulario">
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <div className="contenedor-input">
           <input
             type="text"
             className="input-text"
             placeholder="Name Task ..."
             name="name"
+            value={name}
+            onChange={handleChange}
           />
         </div>
 
