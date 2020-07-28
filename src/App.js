a746d1d017f4dd1315de769736341816d27c0306
alejandroPolak <alejandro_polak@hotmail.com>
2020-07-28 09:56:04 -0300

    [Create Acount and authentication from REACT] (protect Component) create a componente conteinter (components/routes/privateRoute.js) the component show if it's authenticated (App) -> import privateRote change Route for  PrivateRoute

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import TaskState from './context/tasks/taskState'
 import AlertState from './context/alerts/alertState'
 import AuthState from './context/authentication/authState'
 import tokenAuth from './config/tokenAuth.config'
+import PrivateRoute from './components/rutes/PrivateRute'
+
 //review if have a token
 const token= localStorage.getItem('token')
 if(token){
@@ -26,7 +28,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Login} />
                 <Route exact path="/new-account" component={NewAccount} />
-                <Route exact path="/projects" component={Projects} />
+                <PrivateRoute exact path="/projects" component={Projects} />
               </Switch>
             </Router>
           </AuthState>
