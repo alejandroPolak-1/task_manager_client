84c78bf299a40d198680d6bb9a67f6fd0fd6ab9e
alejandroPolak <alejandro_polak@hotmail.com>
2021-02-22 12:45:08 -0300

    PWA

--- src/context/tasks/taskContext.js
+++ src/context/tasks/taskContext.js
@@ -1,5 +1,5 @@
-import { createContext } from 'react'
+import {createContext} from 'react';
 
-const taskContext = createContext()
+const projectContext = createContext();
 
-export default taskContext
+export default projectContext;
